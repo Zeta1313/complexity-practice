672ca23de52a99cb765c5f7fc52fbbd318bf9de3
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2025-10-16 14:26:25 -0700

    Third try

--- src/Main.java
+++ src/Main.java
@@ -71,6 +71,7 @@ public class Main {
 
   // Assume that the largest number is no bigger than the length
   // of the array
+  //O(n^2) where n is the length of nums
   public static void computeAllFactorials(int[] nums) {
     for(int num : nums) {
         int result = computeFactorial(num);
@@ -81,7 +82,7 @@ public class Main {
 
   // assume that each String is bounded by a constant length
   // The time complexity is:
-  // YOUR ANSWER HERE
+  // O(n) where n is the length of arr
   public static void checkIfContainedArrayList(ArrayList<String> arr, String target) {
     if (arr.contains(target)) {
         System.out.println(target + " is present in the list");
